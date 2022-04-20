425b298e39049a491686c0ca5d30827984855569
shinDongMin1 <97037751+shinDongMin1@users.noreply.github.com>
2022-04-20 13:36:58 +0900

    신동민

---
+++ MFC/실습/기말/CH7-1-305039/ChildView.cpp
@@ -0,0 +1,70 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "CH7-1-305039.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	CRect rect;
+	GetClientRect(&rect);
+
+	dc.SetTextColor(RGB(255,0,0));
+	dc.SetBkColor(RGB(255, 255, 0));
+	dc.DrawText(CString("Dream Come TRUE!"), &rect, 0);
+	dc.DrawText(CString("꿈은 이루어진다."), &rect, DT_CENTER | DT_VCENTER| DT_SINGLELINE);
+
+	dc.SetTextAlign(TA_CENTER); //한번 정하면 가운데로감
+	dc.SetTextColor(RGB(0, 0, 255));
+	dc.SetBkColor(RGB(0, 255, 0));
+	dc.TextOutW(rect.right/2 , 3*rect.bottom/4,CString("뜻이 있는 곳에 길이 있다."));
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+	//dc.SetTextAlign(TA_RIGHT);
+	dc.SetTextColor(RGB(0, 0, 0));
+	dc.SetBkColor(RGB(200, 200, 200));
+	dc.TextOutW(rect.right / 2, 1 * rect.bottom / 4, CString("2017305039 신동민"));
+}
+
---
+++ MFC/실습/기말/Ch7-3/Ch7-3View.cpp
@@ -0,0 +1,105 @@
+﻿
+// Ch7-3View.cpp: CCh73View 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Ch7-3.h"
+#endif
+
+#include "Ch7-3Doc.h"
+#include "Ch7-3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CCh73View
+
+IMPLEMENT_DYNCREATE(CCh73View, CView)
+
+BEGIN_MESSAGE_MAP(CCh73View, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CCh73View 생성/소멸
+
+CCh73View::CCh73View() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CCh73View::~CCh73View()
+{
+}
+
+BOOL CCh73View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CCh73View 그리기
+
+void CCh73View::OnDraw(CDC* /*pDC*/)
+{
+	CCh73Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CCh73View 인쇄
+
+BOOL CCh73View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CCh73View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CCh73View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CCh73View 진단
+
+#ifdef _DEBUG
+void CCh73View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CCh73View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CCh73Doc* CCh73View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CCh73Doc)));
+	return (CCh73Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CCh73View 메시지 처리기
