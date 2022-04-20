b1f5f2a0cf8ad44df3c1e9e88e7afa78042597df
shinDongMin1 <97037751+shinDongMin1@users.noreply.github.com>
2022-04-20 13:40:18 +0900

    신동민

---
+++ MFC/실습/기말/Ch9-1-20173050391/Ch9-1-20173050391.h
@@ -0,0 +1,32 @@
+﻿
+// Ch9-1-20173050391.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CCh9120173050391App:
+// 이 클래스의 구현에 대해서는 Ch9-1-20173050391.cpp을(를) 참조하세요.
+//
+
+class CCh9120173050391App : public CWinApp
+{
+public:
+	CCh9120173050391App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCh9120173050391App theApp;
---
+++ MFC/실습/기말/Ch9-2017305039/Ch9-2017305039Dlg.h
@@ -0,0 +1,47 @@
+﻿
+// Ch9-2017305039Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CCh92017305039Dlg 대화 상자
+class CCh92017305039Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CCh92017305039Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	HINSTANCE m_hInstance;
+	int m_nWidth;
+	int m_nHeight;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CH92017305039_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_menuID;
+	int m_menu;
+	afx_msg void OnZoomZoomin();
+	afx_msg void OnZoomZoomout();
+	afx_msg void OnHelpAbout();
+	afx_msg void OnFileExit();
+	afx_msg void OnPictureSports();
+	afx_msg void OnPictureWhale();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnBnClickedButton1();
+};
